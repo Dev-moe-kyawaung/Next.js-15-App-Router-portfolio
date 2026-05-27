78c86e2ac97ea380d7af5bb8749cb27c005bd6a2
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-27 14:00:13 +0630

    Create navigation.ts

---
+++ lib/data/navigation.ts
@@ -0,0 +1,11 @@
+export const navigation = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Experience", href: "/experience" },
+  { name: "Skills", href: "/skills" },
+  { name: "Case Studies", href: "/case-studies" },
+  { name: "Services", href: "/services" },
+  { name: "Blog", href: "/blog" },
+  { name: "Testimonials", href: "/testimonials" },
+  { name: "Contact", href: "/contact" }
+];
